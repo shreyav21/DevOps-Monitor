485a9f6bd10d054fbacfae0a642d2c10fc572397
shreya vedpathak <shreyavedpathak7@gmail.com>
2026-08-08 10:31:00 +0530

    CORS changes

--- backend/src/main/java/com/devopsmonitor/backend/config/WebConfig.java
+++ backend/src/main/java/com/devopsmonitor/backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://dev-ops-monitor-topaz.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
